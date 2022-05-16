bdf8887beff28288b62ebafb0dd1ecba0970681e
Florocloud <105663519+Florocloud@users.noreply.github.com>
2022-05-16 12:20:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title ('my parents new healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+ 
+streamlit.header('Breakfast Menu🥣' )
+streamlit.text('Omega 3 & Blueberry Oatmeal🥗')
+streamlit.text('Kale, Spinach & Rocket Smoothie🐔 ')
+streamlit.text('Hard-Boiled Free-Range Egg🥑🍞')
